3e61d3688f66fae66a8724a8ac394e1d391cf59d
Alex Kizub <kizuba@grangeinsurance.com>
2018-04-12 15:19:25 -0400

    --inspect

--- dbgtest.js
+++ dbgtest.js
@@ -1,18 +1,22 @@
 // https://github.com/nodejs/node/wiki/Using-Eclipse-as-Node-Applications-Debugger
+
+// $ node --debug dbgtest.js
+// DeprecationWarning: `node --debug` and `node --debug-brk` are invalid. Please use `node --inspect` or `node --inspect-brk` instead.
+// $ node --inspect dbgtest.js
+
+
 // dbgtest.js
 
-var sys=require('sys');
 var count = 0;
 
-sys.debug("Starting ...");
-
+console.log("Starting ...");
 
 function timer_tick() {
   count = count+1;
-  sys.debug("Tick count: " + count);
+  console.log("Tick count: " + count);
   if (count === 10) {
     count += 1000;
-    sys.debug("Set break here");
+    console.log("Set break here");
  }
  setTimeout(timer_tick, 1000);
 }
